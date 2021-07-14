a99e3395cc3027afd0ecb1c23f4ae3efe966fbcf
glyceria <glyceria622@i-commtech.com>
2021-07-14 23:08:03 +0900

    기관소개 메뉴 페이지연결

--- src/main/java/kr/or/keepa/smhrc/web/controller/IntrodutionController.java
+++ src/main/java/kr/or/keepa/smhrc/web/controller/IntrodutionController.java
@@ -8,9 +8,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/Introdution")
 public class IntrodutionController {
 
+    //인사말
     @GetMapping("/Greeting")
     public String greeting() {
         System.out.println("greeting");
         return "introdution/greeting";
+    }
+
+    //연혁
+    @GetMapping("/History")
+    public String history() {
+        System.out.println("history");
+        return "introdution/history";
+    }
+
+    //미션과 비젼
+    @GetMapping("/Vision")
+    public String vision() {
+        System.out.println("vision");
+        return "introdution/vision";
+    }
+
+    //조직도
+    @GetMapping("/OrgChart")
+    public String orgChart() {
+        System.out.println("orgChart");
+        return "introdution/organization_chart";
+    }
+
+    //센터로고
+    @GetMapping("/Logo")
+    public String logo() {
+        System.out.println("logo");
+        return "introdution/logo";
+    }
+
+    //찾아오시는 길
+    @GetMapping("/Visit")
+    public String visit() {
+        System.out.println("visit");
+        return "introdution/visit";
     }
 }
