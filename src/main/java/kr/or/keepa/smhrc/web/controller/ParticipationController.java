83d4f477f9b1f2055c79f596accd2cbf6370d887
glyceria <glyceria622@i-commtech.com>
2021-07-15 13:25:59 +0900

    참여광장 index페이지 생성

--- src/main/java/kr/or/keepa/smhrc/web/controller/ParticipationController.java
+++ src/main/java/kr/or/keepa/smhrc/web/controller/ParticipationController.java
@@ -8,30 +8,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/participation")
 public class ParticipationController {
 
+    @GetMapping(value ="")
+    public String root() {
+        return "redirect:/participation/";
+    }
+
+    @GetMapping(value="/")
+    public String index() {
+        return "/participation/index";
+    }
+
     //교육신청
-    @GetMapping("/edu-application")
-    public String application() { return "participation/edu-application"; }
+    @GetMapping(value="/edu-application")
+    public String application() {
+        return "participation/edu-application";
+    }
 
     //이수증출력 및 만족도 조사
-    @GetMapping("/edu-qualification")
+    @GetMapping(value="/edu-qualification")
     public String qualification() {
         return "participation/edu-qualification";
     }
 
     //설문조사
-    @GetMapping("/servey")
+    @GetMapping(value="/servey")
     public String servey() {
         return "participation/servey";
     }
 
     //질문과답변
-    @GetMapping("/qna")
+    @GetMapping(value="/qna")
     public String qna() {
         return "participation/qna";
     }
 
     //자주묻는질문
-    @GetMapping("/faq")
+    @GetMapping(value="/faq")
     public String faq() {
         return "participation/faq";
     }
